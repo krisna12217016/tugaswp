687f8191bb8493bfb497cd8103ce379ad557d127
krisna12217016 <12217016@bsi.ac.id>
2021-09-26 09:25:38 +0700

    modif simpan

---
+++ tugaswp.php
@@ -0,0 +1,13 @@
+<?php
+class tugaswp extand CI_Controller {
+    public function index()
+
+    {
+        echo "<hi> Data Diri </h1>";
+        echo "Nama : Krisna Heru Saputra <br>
+        Tempat Lahir : Bekasi <br>
+        Tanggal Lahir : 23 Agustus 1994 <br>
+        Tempat Tinggal : Rawalumbu <br>
+        Motivasi Kuliah : Upgrade Keahlian";
+    }
+}
